f6c4c191003e49e6e82a941baa2fbf2e18413926
Athip Rooprayochsilp <>
2022-10-23 13:39:03 +0700

    Change base image to ubuntu

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM amazon/aws-cli:latest
+FROM ubuntu:latest
 WORKDIR /space
 
+RUN apt update
+RUN apt install -y groff less curl unzip
+RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+RUN unzip awscliv2.zip
+RUN ./aws/install
 COPY config /root/.aws/config
 COPY credentials /root/.aws/credentials
 COPY script.sh .
 
 ENTRYPOINT ["/space/script.sh"]
-
--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-#!sh
+#!/bin/bash
 
 AWS_ACCESS_KEY=$1
 AWS_SECRET_KEY=$2
@@ -6,14 +6,8 @@ AWS_CODEARTIFACT_DOMAIN=$3
 AWS_CODEARTIFACT_OWNER=$4
 AWS_REGION=$5
 
-echo $AWS_ACCESS_KEY
-echo $AWS_SECRET_KEY
-echo $AWS_CODEARTIFACT_DOMAIN
-echo $AWS_CODEARTIFACT_OWNER
-echo $AWS_REGION
-
-echo "aws_access_key_id = $AWS_ACCESS_KEY" >> /root/.aws/credentials
-echo "aws_secret_access_key = $AWS_SECRET_KEY" >> /root/.aws/credentials
+echo "aws_access_key_id = $AWS_ACCESS_KEY" >> ~/.aws/credentials
+echo "aws_secret_access_key = $AWS_SECRET_KEY" >> ~/.aws/credentials
 TOKEN=`aws codeartifact get-authorization-token --domain $AWS_CODEARTIFACT_DOMAIN --domain-owner $AWS_CODEARTIFACT_OWNER --region $AWS_REGION --query authorizationToken --output text`
 echo "codeartifact_token=$TOKEN" >> $GITHUB_OUTPUT
 
