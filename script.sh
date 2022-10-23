1af54be955034db062006f82ffe1c1d11f132d45
Athip Rooprayochsilp <>
2022-10-23 13:57:08 +0700

    Remove the aws credentials but relies on the aws-actions/configure-aws-credentials instead (in the step before)

--- script.sh
+++ script.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-AWS_ACCESS_KEY=$1
-AWS_SECRET_KEY=$2
-AWS_CODEARTIFACT_DOMAIN=$3
-AWS_CODEARTIFACT_OWNER=$4
-AWS_REGION=$5
+AWS_CODEARTIFACT_DOMAIN=$1
+AWS_CODEARTIFACT_OWNER=$2
+AWS_REGION=$3
 
-echo "aws_access_key_id = $AWS_ACCESS_KEY" >> ~/.aws/credentials
-echo "aws_secret_access_key = $AWS_SECRET_KEY" >> ~/.aws/credentials
 TOKEN=`aws codeartifact get-authorization-token --domain $AWS_CODEARTIFACT_DOMAIN --domain-owner $AWS_CODEARTIFACT_OWNER --region $AWS_REGION --query authorizationToken --output text`
 echo "codeartifact_token=$TOKEN" >> $GITHUB_OUTPUT
 
