332f47be7dd93694a099b4e5a3eab5e4ff3870ee
Athip Rooprayochsilp <>
2022-10-23 11:28:46 +0700

    Add debugging msg

--- script.sh
+++ script.sh
@@ -1,10 +1,16 @@
-#!/bin/bash
+#!sh
 
 AWS_ACCESS_KEY=$1
 AWS_SECRET_KEY=$2
 AWS_CODEARTIFACT_DOMAIN=$3
 AWS_CODEARTIFACT_OWNER=$4
 AWS_REGION=$5
+
+echo $AWS_ACCESS_KEY
+echo $AWS_SECRET_KEY
+echo $AWS_CODEARTIFACT_DOMAIN
+echo $AWS_CODEARTIFACT_OWNER
+echo $AWS_REGION
 
 echo "aws_access_key_id = $AWS_ACCESS_KEY" >> /root/.aws/credentials
 echo "aws_secret_access_key = $AWS_SECRET_KEY" >> /root/.aws/credentials
